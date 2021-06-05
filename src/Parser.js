5fde89ddb02b01c51212da2a068a901291c33d95
Jack Laurence Garay <jacklaurencegaray@@users.noreply.github.com>
2021-06-05 21:50:57 +0200

    Added NumericLiteral to parser

---
+++ src/Parser.js
@@ -0,0 +1,34 @@
+class Parser {
+	/**
+	 * Parses a string into an AST
+	 */
+	parse(string) {
+		this._string = string
+	}
+
+	/** Main entry point
+	 * 
+	 * Program
+	 * 	: NumericLiteral
+	 * ;
+	 */
+
+	Program() {
+		return this.NumericLiteral();
+	}
+
+	/**
+	 * NumericLiteral
+	 * 	: NUMBER
+	 */
+	NumericLiteral() {
+		return {
+			type: 'NumericLiteral',
+			value: Number(this._string)
+		}
+	}
+}
+
+module.exports = {
+	Parser
+}
