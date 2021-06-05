55b0850e038be9aff495bbe5b10781f6f8652129
Jack Laurence Garay <jacklaurencegaray@@users.noreply.github.com>
2021-06-05 22:09:34 +0200

    Prettier

--- src/Parser.js
+++ src/Parser.js
@@ -1,38 +1,40 @@
+const { Tokenizer } = require("./Tokernizer")
+
 class Parser {
-	/**
-	 * Parses a string into an AST
-	 */
-	parse(string) {
-		this._string = string
-		return this.Program();
-	}
+ /**
+  * Parses a string into an AST
+  */
+ parse(string) {
+  this._string = string
+  return this.Program()
+ }
 
-	/** Main entry point
-	 * 
-	 * Program
-	 * 	: NumericLiteral
-	 * ;
-	 */
+ /** Main entry point
+  *
+  * Program
+  * 	: NumericLiteral
+  * ;
+  */
 
-	Program() {
-		return {
-			type: 'Program',
-			body: this.NumericLiteral()
-		}
-	}
+ Program() {
+  return {
+   type: "Program",
+   body: this.NumericLiteral(),
+  }
+ }
 
-	/**
-	 * NumericLiteral
-	 * 	: NUMBER
-	 */
-	NumericLiteral() {
-		return {
-			type: 'NumericLiteral',
-			value: Number(this._string)
-		}
-	}
+ /**
+  * NumericLiteral
+  * 	: NUMBER
+  */
+ NumericLiteral() {
+  return {
+   type: "NumericLiteral",
+   value: Number(this._string),
+  }
+ }
 }
 
 module.exports = {
-	Parser
+ Parser,
 }
--- src/Tokernizer.js
+++ src/Tokernizer.js
@@ -1,39 +1,39 @@
 /**
  * Tokenizer class
- * 
+ *
  * Lazily pulls token from a stream
  */
 
 class Tokenizer {
-	init(string) {
-		this._string = string
-		this._cursor = 0
-	}
+ init(string) {
+  this._string = string
+  this._cursor = 0
+ }
 
-	hasMoreTokens() {
-		return this._cursor < this._string.length
-	}
+ hasMoreTokens() {
+  return this._cursor < this._string.length
+ }
 
-	getNextToken() {
-		if (!this.hasMoreTokens()) {
-			return null;
-		}
+ getNextToken() {
+  if (!this.hasMoreTokens()) {
+   return null
+  }
 
-		const string = this._string.slice(this._cursor);
+  const string = this._string.slice(this._cursor)
 
-		if (!Number.isNan(string[0])) {
-			let number = '';
-			while (!Number.isNan(string[this._cursor])) {
-				number += string[this._cursor++];
-			}
-			return {
-				type: "NUMBER",
-				value: number
-			}
-		}
-	}
+  if (!Number.isNan(string[0])) {
+   let number = ""
+   while (!Number.isNan(string[this._cursor])) {
+    number += string[this._cursor++]
+   }
+   return {
+    type: "NUMBER",
+    value: number,
+   }
+  }
+ }
 }
 
 module.exports = {
-	Tokenizer
+ Tokenizer,
 }
