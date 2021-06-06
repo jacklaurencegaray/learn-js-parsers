529614fc8f43712f2c4a23a2999e87314ea67ae9
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2021-06-06 19:36:49 +0200

    Better tests, generic test runner

---
+++ __tests__/literals-test.js
@@ -0,0 +1,28 @@
+module.exports = (test) => {
+    // NumericLiteral
+    test(`42`, {
+        type: 'Program',
+        body: {
+            type: 'NumericLiteral',
+            value: 42,
+        },
+    })
+
+    // String Literal
+    test(`"hello"`, {
+        type: 'Program',
+        body: {
+            type: 'StringLiteral',
+            value: 'hello',
+        },
+    })
+
+    // String Literal
+    test(`'hello'`, {
+        type: 'Program',
+        body: {
+            type: 'StringLiteral',
+            value: 'hello',
+        },
+    })
+}
--- __tests__/run.js
+++ __tests__/run.js
@@ -3,11 +3,27 @@
  */
 
 const { Parser } = require('../src/Parser')
+const assert = require('assert')
 
 const parser = new Parser()
 
-const program = `'42'`
+// List of tests
+const tests = [require('./literals-test')]
 
-const ast = parser.parse(program)
+function exec() {
+    const program = `42;`
 
-console.log(JSON.stringify(ast, null, 2))
+    const ast = parser.parse(program)
+
+    console.log(JSON.stringify(ast, null, 2))
+}
+
+function test(program, expected) {
+    const ast = parser.parse(program)
+    assert.deepEqual(ast, expected)
+}
+
+exec()
+
+tests.forEach((testRun) => testRun(test))
+console.log('All assertions passed')
